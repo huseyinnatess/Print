d80e237071d1ef52542473f99da506ca143a0222
Hüseyin Ateş <109745789+huseyinnatess@users.noreply.github.com>
2024-04-12 11:49:04 +0300

    Add files via upload

---
+++ color.hpp
@@ -0,0 +1,14 @@
+#ifndef COLOR
+#define COLOR
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define TURQUISE "\033[36m"
+
+#define DEFAULT "\033[0m"
+#define BOLD "\033[1m"
+
+#endif
